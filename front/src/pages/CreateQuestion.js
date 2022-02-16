182cacd632b5905ef828886b131c8d628ddefbb2
Timothée <tnguyen@normandiewebschool.fr>
2022-02-16 16:38:57 +0100

    Home button

--- front/src/pages/CreateQuestion.js
+++ front/src/pages/CreateQuestion.js
@@ -1,7 +1,18 @@
+import Button from "@mui/material/Button";
+import { NavLink } from "react-router-dom";
+
 function CreateQuestion() {
     return (
       <div className="page-creation">
-        
+        <NavLink
+        style={{ textDecoration: "none" }}
+        component={Button}
+        to="/"
+      >
+        <Button className="button-home-question">
+          Home
+        </Button>
+      </NavLink>
       </div>
     );
   }
--- front/src/pages/Game.js
+++ front/src/pages/Game.js
@@ -1,10 +1,20 @@
+import Button from "@mui/material/Button";
+import { NavLink } from "react-router-dom";
+
 function Game() {
-    return (
-      <div className="page-game">
-        
-      </div>
-    );
-  }
-  
-  export default Game;
-  
+  return (
+    <div className="page-game">
+      <NavLink
+        style={{ textDecoration: "none" }}
+        component={Button}
+        to="/"
+      >
+        <Button className="button-home-game">
+          Home
+        </Button>
+      </NavLink>
+    </div>
+  );
+}
+
+export default Game;
