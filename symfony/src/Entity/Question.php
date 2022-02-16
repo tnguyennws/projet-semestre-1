67862333a97169e631fdd06d3d93aa23cf42a34a
Timothée <tnguyen@normandiewebschool.fr>
2022-02-16 10:24:30 +0100

    Edit on database

--- symfony/src/Entity/Question.php
+++ symfony/src/Entity/Question.php
@@ -19,25 +19,13 @@ class Question
     private $image;
 
     #[ORM\Column(type: 'string', length: 255)]
-    private $description;
-
-    #[ORM\Column(type: 'string', length: 255)]
-    private $reponse1;
-
-    #[ORM\Column(type: 'string', length: 255)]
-    private $reponse2;
-
-    #[ORM\Column(type: 'string', length: 255)]
-    private $reponse3;
-
-    #[ORM\Column(type: 'string', length: 255)]
-    private $reponse4;
-
-    #[ORM\Column(type: 'string', length: 255)]
-    private $reponsecorrecte;
-
-    #[ORM\Column(type: 'string', length: 255)]
     private $title;
+
+    #[ORM\Column(type: 'array')]
+    private $answers = [];
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $correctAnswer;
 
     public function getId(): ?int
     {
@@ -56,78 +44,6 @@ class Question
         return $this;
     }
 
-    public function getDescription(): ?string
-    {
-        return $this->description;
-    }
-
-    public function setDescription(string $description): self
-    {
-        $this->description = $description;
-
-        return $this;
-    }
-
-    public function getReponse1(): ?string
-    {
-        return $this->reponse1;
-    }
-
-    public function setReponse1(string $reponse1): self
-    {
-        $this->reponse1 = $reponse1;
-
-        return $this;
-    }
-
-    public function getReponse2(): ?string
-    {
-        return $this->reponse2;
-    }
-
-    public function setReponse2(string $reponse2): self
-    {
-        $this->reponse2 = $reponse2;
-
-        return $this;
-    }
-
-    public function getReponse3(): ?string
-    {
-        return $this->reponse3;
-    }
-
-    public function setReponse3(string $reponse3): self
-    {
-        $this->reponse3 = $reponse3;
-
-        return $this;
-    }
-
-    public function getReponse4(): ?string
-    {
-        return $this->reponse4;
-    }
-
-    public function setReponse4(string $reponse4): self
-    {
-        $this->reponse4 = $reponse4;
-
-        return $this;
-    }
-
-    public function getReponsecorrecte(): ?string
-    {
-        return $this->reponsecorrecte;
-    }
-
-    public function setReponsecorrecte(string $reponsecorrecte): self
-    {
-        $this->reponsecorrecte = $reponsecorrecte;
-
-        return $this;
-    }
-
     public function getTitle(): ?string
     {
         return $this->title;
@@ -136,6 +52,30 @@ class Question
     public function setTitle(string $title): self
     {
         $this->title = $title;
+
+        return $this;
+    }
+
+    public function getAnswers(): ?array
+    {
+        return $this->answers;
+    }
+
+    public function setAnswers(array $answers): self
+    {
+        $this->answers = $answers;
+
+        return $this;
+    }
+
+    public function getCorrectAnswer(): ?string
+    {
+        return $this->correctAnswer;
+    }
+
+    public function setCorrectAnswer(string $correctAnswer): self
+    {
+        $this->correctAnswer = $correctAnswer;
 
         return $this;
     }
